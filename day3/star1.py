9053883e8b03d37b63b02a1baaa01a8d477fc41a
gustav.darpo <Blekholmstorget30>
2017-12-06 09:01:34 +0100

    Day 3 complete finally!

--- day3/star1.py
+++ day3/star1.py
@@ -1,26 +1,26 @@
-from math import sqrt
-
 input = 361527
 
-def closest_odd_integer(number):
-    rounded = round(number)
-    if rounded%2!=0:
-        return rounded
-    else:
-        return rounded-1
+
+def get_neighboor_sum(x, y, positions):
+    if x == 0 and y == 0:
+        return 1
+
+    sum = 0
+    for p in positions:
+        if abs(x-p[0]) <= 1 and abs(y-p[1]) <= 1:
+            sum += p[2]
+
+    return sum
+
 
 def circular_coord(pos):
-    prev_d = closest_odd_integer(sqrt(pos))
-    rest = pos - prev_d * prev_d
-    d = prev_d + 2
-
-
-def circular_coord_2(pos):
     x = 0
     y = 0
     dir = 3
     d = 1
     dir_counter = 1
+
+    positions = []
     for p in range(1, pos):
         dir_counter += 1
 
@@ -42,7 +42,42 @@ def circular_coord_2(pos):
 
     return x, y
 
+def star2(pos):
+    x = 0
+    y = 0
+    dir = 3
+    d = 1
+    dir_counter = 1
 
-pos = circular_coord_2(361527)
+    positions = []
+    for p in range(1, pos):
+        sum = get_neighboor_sum(x, y, positions)
+        positions.append((x, y, sum))
+        if sum > pos:
+            return sum
+
+        dir_counter += 1
+
+        if dir == 0:
+            y += 1
+        elif dir == 1:
+            x -= 1
+        elif dir == 2:
+            y -= 1
+        elif dir == 3:
+            x += 1
+
+        if dir_counter >= d:
+            dir = (dir + 1) % 4
+            dir_counter = 0
+
+            if dir == 3 or dir == 1:
+                d += 1
+
+    return x, y
+
+
+pos = circular_coord(361527)
 print("At pos " + str(pos) + " and distance " + str(abs(pos[0]) + abs(pos[1])))
 
+print(star2(361527))
