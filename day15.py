3d96d8cbd51b78c6ce39611d1a3dcbdf493e6ca3
gustav.darpo <Blekholmstorget30>
2017-12-15 08:54:41 +0100

    day 14 star 2

--- day15.py
+++ day15.py
@@ -1,3 +1,38 @@
-def read_input(file):
-    with open('day14.txt') as fp:
-        return [line for line in fp]
+
+def star1(start_a, start_b):
+    a = start_a
+    b = start_b
+
+    matches = 0
+    for i in range(40000000):
+        if a & 0xFFFF == b & 0xFFFF:
+            matches += 1
+
+        a = (a * 16807) % 2147483647
+        b = (b * 48271) % 2147483647
+
+    return matches
+
+
+def star2(a, b):
+    matches = 0
+    for i in range(5000000):
+        a = generate(a, 16807, 4)
+        b = generate(b, 48271, 8)
+
+        if a & 0xFFFF == b & 0xFFFF:
+            matches += 1
+
+    return matches
+
+
+def generate(number, factor, div):
+    number = (number * factor) % 2147483647
+    while number % div != 0:
+        number = (number * factor) % 2147483647
+
+    return number
+
+
+#print(star1(873, 583))
+print(star2(873, 583))
---
+++ day16.py
@@ -0,0 +1,3 @@
+def read_input(file):
+    with open('day14.txt') as fp:
+        return [line for line in fp]
